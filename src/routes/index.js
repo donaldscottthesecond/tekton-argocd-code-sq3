b8bb7e0fbe88b99c82d7c2ccddd05d2c20a71511
Donald Scott II <brasco1903@hotmail.com>
2021-03-31 10:42:45 -0500

    date

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Cloud Native demo March 31st 2021 !' });
+  res.render('index', { title: 'Cloud Native demo March 31st 2021 #' });
 });
 
 module.exports = router;
