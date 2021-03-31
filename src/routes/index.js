05c97a2b31dfbe88285822a9274fdc6ee64f273a
sparkpark <spark.park@ibm.com>
2021-03-31 12:06:25 -0700

    Testing push for Spark

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Cloud Native demo March 31st 2021 3' });
+  res.render('index', { title: 'Testing for Spark' });
 });
 
 module.exports = router;
